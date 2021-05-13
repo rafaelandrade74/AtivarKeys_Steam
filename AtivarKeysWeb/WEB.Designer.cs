6b31ccf9884863b9dbf6a74a514c9ce9bb49e64f
Rafael <rafael_andrade74@hotmail.com>
2021-05-13 11:43:20 -0300

    Second commit

---
+++ AtivarKeysWeb/WEB.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace AtivarKeysWeb
+{
+    partial class FormWeb
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblAtivados = new System.Windows.Forms.Label();
+            this.lblNOK = new System.Windows.Forms.Label();
+            this.lblQtdKeys = new System.Windows.Forms.Label();
+            this.START = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTempo = new System.Windows.Forms.Label();
+            this.BTN_START_KEYS = new System.Windows.Forms.Button();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BTN_STOP_KEYS = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtGuard = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblAtivados
+            // 
+            this.lblAtivados.AutoSize = true;
+            this.lblAtivados.Location = new System.Drawing.Point(95, 149);
+            this.lblAtivados.Name = "lblAtivados";
+            this.lblAtivados.Size = new System.Drawing.Size(60, 13);
+            this.lblAtivados.TabIndex = 1;
+            this.lblAtivados.Text = "Ativados: 0";
+            // 
+            // lblNOK
+            // 
+            this.lblNOK.AutoSize = true;
+            this.lblNOK.Location = new System.Drawing.Point(95, 173);
+            this.lblNOK.Name = "lblNOK";
+            this.lblNOK.Size = new System.Drawing.Size(78, 13);
+            this.lblNOK.TabIndex = 2;
+            this.lblNOK.Text = "Não Ativado: 0";
+            // 
+            // lblQtdKeys
+            // 
+            this.lblQtdKeys.AutoSize = true;
+            this.lblQtdKeys.Location = new System.Drawing.Point(95, 195);
+            this.lblQtdKeys.Name = "lblQtdKeys";
+            this.lblQtdKeys.Size = new System.Drawing.Size(47, 13);
+            this.lblQtdKeys.TabIndex = 3;
+            this.lblQtdKeys.Text = "KEYS: 0";
+            // 
+            // START
+            // 
+            this.START.Location = new System.Drawing.Point(67, 243);
+            this.START.Name = "START";
+            this.START.Size = new System.Drawing.Size(51, 25);
+            this.START.TabIndex = 4;
+            this.START.Text = "LOGIN";
+            this.START.UseVisualStyleBackColor = true;
+            this.START.Click += new System.EventHandler(this.START_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 287);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "TEMPO:";
+            // 
+            // lblTempo
+            // 
+            this.lblTempo.AutoSize = true;
+            this.lblTempo.Location = new System.Drawing.Point(65, 287);
+            this.lblTempo.Name = "lblTempo";
+            this.lblTempo.Size = new System.Drawing.Size(34, 13);
+            this.lblTempo.TabIndex = 6;
+            this.lblTempo.Text = "30:00";
+            // 
+            // BTN_START_KEYS
+            // 
+            this.BTN_START_KEYS.Location = new System.Drawing.Point(147, 243);
+            this.BTN_START_KEYS.Name = "BTN_START_KEYS";
+            this.BTN_START_KEYS.Size = new System.Drawing.Size(51, 25);
+            this.BTN_START_KEYS.TabIndex = 7;
+            this.BTN_START_KEYS.Text = "START";
+            this.BTN_START_KEYS.UseVisualStyleBackColor = true;
+            this.BTN_START_KEYS.Click += new System.EventHandler(this.BTN_START_KEYS_Click);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(98, 30);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 8;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(98, 66);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(100, 20);
+            this.txtSenha.TabIndex = 9;
+            this.txtSenha.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Senha";
+            // 
+            // BTN_STOP_KEYS
+            // 
+            this.BTN_STOP_KEYS.Location = new System.Drawing.Point(147, 243);
+            this.BTN_STOP_KEYS.Name = "BTN_STOP_KEYS";
+            this.BTN_STOP_KEYS.Size = new System.Drawing.Size(51, 25);
+            this.BTN_STOP_KEYS.TabIndex = 12;
+            this.BTN_STOP_KEYS.Text = "STOP";
+            this.BTN_STOP_KEYS.UseVisualStyleBackColor = true;
+            this.BTN_STOP_KEYS.Visible = false;
+            this.BTN_STOP_KEYS.Click += new System.EventHandler(this.BTN_STOP_KEYS_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "STEAMGUARD";
+            // 
+            // txtGuard
+            // 
+            this.txtGuard.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtGuard.Location = new System.Drawing.Point(98, 103);
+            this.txtGuard.MaxLength = 5;
+            this.txtGuard.Name = "txtGuard";
+            this.txtGuard.Size = new System.Drawing.Size(100, 20);
+            this.txtGuard.TabIndex = 13;
+            // 
+            // FormWeb
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 321);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtGuard);
+            this.Controls.Add(this.BTN_STOP_KEYS);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.BTN_START_KEYS);
+            this.Controls.Add(this.lblTempo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.START);
+            this.Controls.Add(this.lblQtdKeys);
+            this.Controls.Add(this.lblNOK);
+            this.Controls.Add(this.lblAtivados);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormWeb";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SteamActivationBulk";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormWeb_FormClosing);
+            this.Load += new System.EventHandler(this.FormWeb_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblAtivados;
+        private System.Windows.Forms.Label lblNOK;
+        private System.Windows.Forms.Label lblQtdKeys;
+        private System.Windows.Forms.Button START;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTempo;
+        private System.Windows.Forms.Button BTN_START_KEYS;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BTN_STOP_KEYS;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtGuard;
+    }
+}
+
---
+++ AtivarKeysWeb/WEB.cs
@@ -0,0 +1,298 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace AtivarKeysWeb
+{
+    public partial class FormWeb : System.Windows.Forms.Form
+    {
+        public FormWeb()
+        {
+            InitializeComponent();
+
+        }
+        public static IWebDriver driver = new ChromeDriver(@".\");
+        static Thread Login;
+        static Thread Ler;
+        static bool Parar = false;
+
+        static string[] erros =
+        {
+            "Houve muitas tentativas de ativação realizadas a partir desta conta ou endereço IP. Por favor, aguarde e tente usar o seu código de produto novamente mais tarde.",
+            "O código de produto inserido já foi ativado por uma outra conta Steam, não podendo ser usado novamente. Por favor, entre em contato com a loja da qual adquiriu o código para assistência.",
+            "Esta conta Steam já possui o(s) produto(s) contido(s) nesta oferta. Para acessá-los, abra a biblioteca no cliente Steam."
+        };
+        private static string usuario;
+        private static string senha;
+        private static string sGuard;
+        private static int minutos = 0;
+        private static int segundos = 0;
+        private static int qtdkeys = 0;
+        private static int qtdkeysAt = 0;
+        private static int qtdkeysNat = 0;
+
+        private void START_Click(object sender, EventArgs e)
+        {
+            usuario = txtUsuario.Text;
+            senha = txtSenha.Text;
+            sGuard = txtGuard.Text;
+            Login = new Thread(new ThreadStart(LogarSteam));
+            Login.Start();
+
+        }
+
+        private void BTN_START_KEYS_Click(object sender, EventArgs e)
+        {
+            BTN_START_KEYS.Visible = false;
+            BTN_STOP_KEYS.Visible = true;
+            Parar = false;
+            Ler = new Thread(new ThreadStart(LerKeys));
+            Ler.Start();
+
+        }
+
+        public static void GravarLog(string Linha)
+        {
+            if (!File.Exists(@".\log.txt"))
+            {
+                var oTxt = File.CreateText("Log.txt");
+                oTxt.Close();
+            }
+
+            using (StreamWriter w = File.AppendText("log.txt"))
+            {
+                w.WriteLine("[{0}] - {1}", DateTime.Today.ToLongDateString(), Linha);
+            }
+
+            //using (StreamReader r = File.OpenText("log.txt"))
+            //{
+            //    DumpLog(r);
+            //}
+        }
+        public static void LerKeys()
+        {
+            if (File.Exists(@".\keys.txt"))
+            {
+
+
+                var Arq_Keys = File.OpenText(@".\keys.txt").ReadToEnd();
+                var Linha = Arq_Keys.Split(new[] { "\r\n" }, StringSplitOptions.None);
+
+                qtdkeys = Linha.Length;
+                foreach (string Li in Linha)
+                {
+
+                    string aErro = string.Empty;
+                    do
+                    {
+                        if (aErro == erros[0])
+                        {
+                            for (minutos = 0; minutos < 40; minutos++)
+                            {
+                                for (segundos = 0; segundos <= 60; segundos++)
+                                {
+                                    Thread.Sleep(1000);
+                                    if (Parar) break;
+                                }
+                                if (Parar) break;
+                            }
+                        }
+
+                        aErro = Ativar(Li);
+                    } while (aErro == erros[0] && !Parar);
+                    if (aErro == erros[1])
+                    {
+                        GravarLog("Key: " + Li.Replace("\r", "") + " - Já Ativado");
+                        //oTxt.WriteLine("Key: {0} - Já Ativado", Li);
+                        qtdkeysNat = qtdkeysNat + 1;
+                        for (minutos = 0; minutos < 5; minutos++)
+                        {
+                            for (segundos = 0; segundos <= 60; segundos++)
+                            {
+                                Thread.Sleep(1000);
+                                if (Parar) break;
+                            }
+                            if (Parar) break;
+                        }
+
+                    }
+                    else if (aErro == "Sucesso")
+                    {
+                        GravarLog("Key: " + Li.Replace("\r", "") + " - Ativado");
+                        //oTxt.WriteLine("Key: {0} - Ativado", Li);
+                        qtdkeysAt = qtdkeysAt + 1;
+                        for (minutos = 0; minutos < 5; minutos++)
+                        {
+                            for (segundos = 0; segundos <= 60; segundos++)
+                            {
+                                Thread.Sleep(1000);
+                                if (Parar) break;
+                            }
+                            if (Parar) break;
+                        }
+
+                    }
+                    else if (aErro == "Deslogado")
+                    {
+                        GravarLog("Key: " + Li.Replace("\r", "") + " - Deslogado");
+                        Parar = true;
+                        //oTxt.WriteLine("Key: {0} - Ativado", Li);
+                        qtdkeysAt = qtdkeysAt + 1;
+                        for (minutos = 0; minutos < 5; minutos++)
+                        {
+                            for (segundos = 0; segundos <= 60; segundos++)
+                            {
+                                Thread.Sleep(1000);
+                                if (Parar) break;
+                            }
+                            if (Parar) break;
+                        }
+
+                    }
+                    else if (aErro == erros[2])
+                    {
+                        GravarLog("Key: " + Li.Replace("\r", "") + " - já possui o produto");
+                        //oTxt.WriteLine("Key: {0} - já possui o produto", Li);
+                        qtdkeysNat = qtdkeysNat + 1;
+                        for (minutos = 0; minutos < 5; minutos++)
+                        {
+                            for (segundos = 0; segundos <= 60; segundos++)
+                            {
+                                Thread.Sleep(1000);
+                                if (Parar) break;
+                            }
+                            if (Parar) break;
+                        }
+                    }
+                    else
+                    {
+                        GravarLog("Key: " + Li.Replace("\r", "") + " - outro Problema");
+                        //oTxt.WriteLine("Key: {0} - outro Problema", Li);
+                        qtdkeysNat = qtdkeysNat + 1;
+                        for (minutos = 0; minutos < 5; minutos++)
+                        {
+                            for (segundos = 0; segundos <= 60; segundos++)
+                            {
+                                Thread.Sleep(1000);
+                                if (Parar) break;
+                            }
+                            if (Parar) break;
+                        }
+                    }
+
+
+                    if (Parar)
+                    {
+                        break;
+                    }
+
+                }
+
+
+            }
+            Ler.Abort();
+        }
+        public static void LogarSteam()
+        {
+            driver.Navigate().GoToUrl("https://store.steampowered.com/login/");
+
+            if (driver.Title.Equals("Iniciar sessão"))
+            {
+                //login
+                driver.FindElement(By.Id("input_username")).SendKeys(usuario);
+                driver.FindElement(By.Id("input_password")).SendKeys(senha);
+                driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
+                driver.FindElement(By.TagName("button")).Click();
+                driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
+                if (!string.IsNullOrWhiteSpace(sGuard) && !string.IsNullOrEmpty(sGuard))
+                {
+                    //steam Guard
+                    Thread.Sleep(5000);
+                    driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
+                    driver.FindElement(By.Id("twofactorcode_entry")).SendKeys(sGuard);
+                    driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
+                    Thread.Sleep(2000);
+                    driver.FindElement(By.XPath("//*[@id='twofactorcode_entry']")).Click();
+                }
+            }
+            //twofactorcode_entry 
+            Login.Abort();
+        }
+        public static string Ativar(string key)
+        {
+            driver.Navigate().GoToUrl("https://store.steampowered.com/account/registerkey?key=" + key);
+
+            if (driver.Title.Equals("Ativar um produto no Steam"))
+            {
+                if (!driver.FindElement(By.Id("accept_ssa")).Selected)
+                {
+                    driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
+                    driver.FindElement(By.Id("accept_ssa")).Click();
+                }
+                driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
+                driver.FindElement(By.Id("register_btn")).Click();
+                Thread.Sleep(5000);
+                foreach (string texto in erros)
+                {
+                    if (driver.FindElement(By.Id("error_display")).Text.Equals(texto))
+                    {
+                        return driver.FindElement(By.Id("error_display")).Text;
+                    }
+                }
+                return "Sucesso";
+            }
+            else
+            {
+                return "Deslogado";
+            }
+        }
+
+        private void FormWeb_FormClosing(object sender, System.Windows.Forms.FormClosingEventArgs e)
+        {
+            driver.Quit();
+            try
+            {
+                Ler.Abort();
+                Login.Abort();
+            }
+            catch
+            {
+
+            }
+
+
+        }
+
+        private void BTN_STOP_KEYS_Click(object sender, EventArgs e)
+        {
+            Parar = true;
+            BTN_START_KEYS.Visible = true;
+            BTN_STOP_KEYS.Visible = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblTempo.Text = minutos + ":" + segundos;
+            lblQtdKeys.Text = "Keys: " + qtdkeys;
+            lblAtivados.Text = "Ativados: " + qtdkeysAt;
+            lblNOK.Text = "Não Ativado: " + qtdkeysNat;
+        }
+
+
+        public static void DumpLog(StreamReader r)
+        {
+            string line;
+            while ((line = r.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        private void FormWeb_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
